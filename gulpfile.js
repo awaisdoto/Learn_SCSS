1db1be2a51f270a6d3840a7fd2ed655d64c0609b
awais-saleem <awais@techloyce.com>
2023-08-08 20:08:03 +0500

    added scss files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const {src, dest, watch ,series} = require('gulp');
 const sass= require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('index.scss')
+    return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 
@@ -10,7 +10,7 @@ function buildStyles() {
 
 function watchTask()
 {
-    watch(['index.scss'],buildStyles);
+    watch(['*.scss'],buildStyles);
 
 }
 
